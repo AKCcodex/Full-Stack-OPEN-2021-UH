3ed32d7c763046681d0366eb14727284c9cacdbb
AKC.devel <56405323+AKCcodex@users.noreply.github.com>
2021-06-14 21:39:33 -0700

    update

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,61 +1,49 @@
 import React, { useState } from 'react'
-import Person from './components/Person'
+
 const App = () => {
   const [ persons, setPersons ] = useState([
-    
+    { name: 'Arto Hellas' }
   ]) 
-  const [newPerson, setNewPerson] = useState(
-    'a new Person...'
-  ) 
+  const [ newName, setNewName ] = useState('')
   const addPerson = (event) => {
     event.preventDefault()
-    const personObject = {
-      content: newPerson,
+    const nameObject = {
+      content: newName,
       date: new Date().toISOString(),
+      important: Math.random() < 0.5,
       id: persons.length + 1,
-  }
-  setPersons(persons.concat(personObject))
-  setNewPerson('')
-}
-  const handlePersonChange = (event) => {
-    console.log(event.target.value)
-    setNewPerson(event.target.value)
-  } 
-  
-  
- 
-  
+    }
     
   
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+
+  }
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  
+  }
   
   return (
-    
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newPerson  }
-          onChange={handlePersonChange}
+          name: <input value={newName} 
+          onChange={handleNameChange}
           />
+          
         </div>
-        
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(person => 
-          <Person key={person.id} person={person} />
-        )}
-      </ul>
+      
     </div>
     
-    
-  
   )
-
-  
 }
 
 
--- part2/phonebook/src/components/Person.js
+++ part2/phonebook/src/components/Person.js
@@ -1,9 +1,7 @@
 import React from 'react'
-
 const Person = ({ person }) => {
   return (
     <li>{person.content}</li>
-  )
+  )  
 }
-
 export default Person
